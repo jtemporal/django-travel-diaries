10dd208587e64d5c61b520ecab058507329f9889
Jessica Temporal <jtemporal@users.noreply.github.com>
2025-03-09 21:26:11 +0000

    Creat view for location pages

--- map/views.py
+++ map/views.py
@@ -1,6 +1,8 @@
 import folium
 from django.shortcuts import render
+from django.utils import timezone
 from .models import Location
+from blog.models import Post
 
 
 def map_page(request):
@@ -17,3 +19,16 @@ def map_page(request):
         ).add_to(folium_map)
     folium_map.save("map/templates/map/map.html")
     return render(request, "map/map.html")
+
+
+def post_list_by_location(request, name):
+    location = Location.objects.get(name__iexact=name)
+
+    posts = Post.objects.filter(
+        location=location, published_date__lte=timezone.now()
+    ).order_by('-published_date')
+
+    return render(
+        request, 'map/post_list_by_location.html',
+        {"posts": posts, "location": location}
+    )
