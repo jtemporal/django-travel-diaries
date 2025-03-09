26da44d5f8f6a911c42a8bde1fd9f50417bf23b5
Jessica Temporal <jtemporal@users.noreply.github.com>
2025-03-09 20:47:22 +0000

    Adds location to blog posts

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
+from map.models import Location
 
 
 class Post(models.Model):
@@ -9,6 +10,7 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    location = models.ForeignKey(Location, related_name='place', blank=True, null=True, on_delete=models.PROTECT)
 
     def publish(self):
         self.published_date = timezone.now()
